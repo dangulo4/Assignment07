0d98a9341373188b67cac3303fecea569dbcfd87
Daniel Angulo <dangulo4@gmail.com>
2023-04-11 17:11:59 -0600

    add employee and set local storage

--- js/script-ems.js
+++ js/script-ems.js
@@ -1,57 +1,57 @@
 // CREATE AN ARRAY OF EMPLOYEES
-let storage;
-let employees = [
+let employee = [];
+let employees = JSON.parse(localStorage.getItem('employees')) || [
   ['05698474', 'Daniel Angulo', 8978, 'dangulo4@gmail.com', 'Engineering'],
   ['89785693', 'Hank Garland', 1951, 'hank@mail.com', 'Sales'],
   ['64578963', 'Grady Martin', 5619, 'grady@mail.com', 'Administrative'],
-  ['56789632', 'Holly Smith', 0920, 'holly@mail.com', 'Quality Assurance'],
+  ['56789632', 'Holly Smith', 1920, 'holly@mail.com', 'Quality Assurance'],
   ['78000247', 'James Crook', 3216, 'james@mail.com', 'Sales'],
 ];
 // CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS
 localStorage.setItem('employees', JSON.stringify(employees));
-const displayEmolyeeList = () => {
+const displayEmployeeList = () => {
   // IF THERE ARE NO employees IN ARRAY, CHECK STORAGE
   if (employees.length === 0) {
     // GET employees FROM STORAGE OR EMPTY STRING IF STORAGE IS EMPTY
     storage = localStorage.getItem('employees') || '';
-    // IF NOT EMPTY, CONVERT TO ARRAY AND STORE IN employees VARIABLE
-    // IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY
-    if (storage.length > 0) {
-      employees = storage;
-    }
   }
 };
 
 // GET DOM ELEMENTS
 const $ = (id) => {
-  return document.querySelector(id);
+  return document.getElementById(id);
 };
 // BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
 let table = document.querySelector('tbody');
-// for (let i = 0; i < employees.length; i++) {
+let form = document.querySelector('form');
 // DISPLAY employees
-table.innerHTML = employees.map((item) => {
-  return `<tbody>
-      <tr>
-        <td>${item[0]}</td>
-        <td>${item[1]}</td>
-        <td>${item[2]}</td>
-        <td>${item[3]}</td>
-        <td>${item[4]}</td>
-      </tr>
-    </tbody>`;
-});
-// }
+buildGrid(employees);
 
 // ADD EMPLOYEE
 form.addEventListener('submit', (e) => {
   // PREVENT FORM SUBMISSION
+  e.preventDefault();
   // GET THE VALUES FROM THE TEXT BOXES
-  // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
-  // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
-  // BUILD THE GRID
+  if ($('id').value !== '') {
+    // ADD employee TO ARRAY
+    employee.push($('id').value);
+    employee.push($('name').value);
+    employee.push($('extension').value);
+    employee.push($('email').value);
+    employee.push($('department').value);
+    // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
+    employees.push(employee);
+    // ADD employee TO STRORAGE
+    localStorage.setItem('employees', JSON.stringify(employees));
+    // SET FOCUS BACK TO THE ID TEXT BOX
+    $('id').focus();
+    // BUILD THE GRID
+    buildGrid(employees);
+  } else {
+    alert('Please enter an Employee.');
+  }
   // RESET THE FORM
-  // SET FOCUS BACK TO THE ID TEXT BOX
+  form.reset();
 });
 
 // DELETE EMPLOYEE
@@ -63,12 +63,25 @@ empTable.addEventListener('click', (e) => {
 });
 
 // BUILD THE EMPLOYEES GRID
-function buildGrid() {
+function buildGrid(employees) {
   // REMOVE THE EXISTING SET OF ROWS BY REMOVING THE ENTIRE TBODY SECTION
-  // REBUILD THE TBODY FROM SCRATCH
-  // LOOP THROUGH THE ARRAY OF EMPLOYEES
-  // REBUILDING THE ROW STRUCTURE
-  // BIND THE TBODY TO THE EMPLOYEE TABLE
+  // LOOP THROUGH THE ARRAY OF EMPLOYEES AND BIND THE TBODY TO THE EMPLOYEE TABLE
   // UPDATE EMPLOYEE COUNT
   // STORE THE ARRAY IN STORAGE
+  table.innerHTML = employees.map((item) => {
+    // REBUILD THE TBODY FROM SCRATCH AND ROW STRUCTURE
+    return `<tbody>
+      <tr>
+        <td>${item[0]}</td>
+        <td>${item[1]}</td>
+        <td>${item[2]}</td>
+        <td>${item[3]}</td>
+        <td>${item[4]}</td>
+      </tr>
+    </tbody>`;
+  });
+  if (employees.length === 0) {
+    // GET employees FROM STORAGE OR EMPTY STRING IF STORAGE IS EMPTY
+    employees = localStorage.getItem('employees') || '';
+  }
 }
